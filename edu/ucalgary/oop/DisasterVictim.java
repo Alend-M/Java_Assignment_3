f9a643896236516998e857c87182b3f77f4c4a6b
MarcusLieu <marcus.lieu4@gmail.com>
2024-02-15 23:03:28 -0700

    passed debug tests

--- edu/ucalgary/oop/DisasterVictim.java
+++ edu/ucalgary/oop/DisasterVictim.java
@@ -2,6 +2,7 @@ package edu.ucalgary.oop;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,15 +12,17 @@ public class DisasterVictim {
     private String dateOfBirth;
     private String comments;
     private int ASSIGNED_SOCIAL_ID;
-    private ArrayList<MedicalRecord> medicalRecords;
-    private ArrayList<FamilyRelation> familyConnections;
+    private MedicalRecord[] medicalRecords;
+    private FamilyRelation[] familyConnections;
     private String ENTRY_DATE;
-    private ArrayList<Supply> personalBelongings;
+    private Supply[] personalBelongings;
     private String gender;
-    private int counter;
+    private static int counter;
 
     public DisasterVictim(String firstName, String ENTRY_DATE) throws IllegalArgumentException{
         this.firstName = firstName;
+        this.ASSIGNED_SOCIAL_ID = counter;
+        counter++;
         Pattern entry_date_pat = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}$");
         Matcher myMatcher = entry_date_pat.matcher(ENTRY_DATE);
         if(myMatcher.find()) {
@@ -42,19 +45,19 @@ public class DisasterVictim {
     public String getComments() {
         return this.comments;
     }
-    public ArrayList<MedicalRecord> getMedicalRecords() {
+    public MedicalRecord[] getMedicalRecords() {
         return this.medicalRecords;
     }
-    public String getENTRY_DATE() {
+    public String getEntryDate() {
         return this.ENTRY_DATE;
     }
-    public int getASSIGNED_SOCIAL_ID() {
+    public int getAssignedSocialID() {
         return this.ASSIGNED_SOCIAL_ID;
     }
-    public ArrayList<Supply> getPersonalBelongings() {
+    public Supply[] getPersonalBelongings() {
         return this.personalBelongings;
     }
-    public ArrayList<FamilyRelation> getFamilyConnections() {
+    public FamilyRelation[] getFamilyConnections() {
         return this.familyConnections;
     }
     public String getGender() {
@@ -79,31 +82,76 @@ public class DisasterVictim {
     public void setComments(String comments) {
         this.comments = comments;
     }
-    public void setMedicalRecords(ArrayList<MedicalRecord> medicalRecords) {
+    public void setMedicalRecords(MedicalRecord[] medicalRecords) {
         this.medicalRecords = medicalRecords;
     }
-    public void setPersonalBelongings(ArrayList<Supply> supplies) {
+    public void setPersonalBelongings(Supply[] supplies) {
         this.personalBelongings = supplies;
     }
-    public void setFamilyConnections(ArrayList<FamilyRelation> familyConnections) {
+    public void setFamilyConnections(FamilyRelation[] familyConnections) {
         this.familyConnections = familyConnections;
     }
+    
     public void setGender(String gender) {
         this.gender = gender;
     }
     public void addPersonalBelonging(Supply supply) {
-        this.personalBelongings.add(supply);
+        if (this.personalBelongings == null) {
+            this.personalBelongings = new Supply[]{supply};
+        } else {
+            Supply[] newArr = Arrays.copyOf(this.getPersonalBelongings(), this.getPersonalBelongings().length+1);
+            newArr[newArr.length-1] = supply;
+            this.personalBelongings = newArr;
+        }
     }
     public void removePersonalBelonging(Supply supply) {
-        this.personalBelongings.remove(supply);
+        int indexToRemove = -1;
+        for (int i = 0; i < this.getPersonalBelongings().length; i++) {
+            if (this.getPersonalBelongings()[i].equals(supply)) {
+                indexToRemove = i;
+                break;
+            }
+        }
+        if (indexToRemove != -1) {
+            Supply[] newArray = new Supply[this.getPersonalBelongings().length - 1];
+            System.arraycopy(this.getPersonalBelongings(), 0, newArray, 0, indexToRemove);
+            System.arraycopy(this.getPersonalBelongings(), indexToRemove + 1, newArray, indexToRemove, newArray.length - indexToRemove);
+            this.personalBelongings = newArray;
+        } 
     }
     public void addFamilyConnection(FamilyRelation familyConnection) {
-        this.familyConnections.add(familyConnection);
+        if (this.familyConnections == null) {
+            this.familyConnections = new FamilyRelation[]{familyConnection};
+        }
+        else {
+            FamilyRelation[] newArr = Arrays.copyOf(this.getFamilyConnections(), this.getFamilyConnections().length+1);
+            newArr[newArr.length-1] = familyConnection;
+            this.familyConnections = newArr;
+        }
     }
     public void removeFamilyConnection(FamilyRelation familyConnection) {
-        this.familyConnections.remove(familyConnection);
+        int indexToRemove = -1;
+        for (int i = 0; i < this.getFamilyConnections().length; i++) {
+            if (this.getFamilyConnections()[i].equals(familyConnection)) {
+                indexToRemove = i;
+                break;
+            }
+        }
+        if (indexToRemove != -1) {
+            FamilyRelation[] newArray = new FamilyRelation[this.getFamilyConnections().length - 1];
+            System.arraycopy(this.getFamilyConnections(), 0, newArray, 0, indexToRemove);
+            System.arraycopy(this.getFamilyConnections(), indexToRemove + 1, newArray, indexToRemove, newArray.length - indexToRemove);
+            this.familyConnections = newArray;
+        } 
     }
     public void addMedicalRecord(MedicalRecord medicalRecord) {
-        this.medicalRecords.add(medicalRecord);
+        if (this.medicalRecords == null) {
+            this.medicalRecords = new MedicalRecord[]{medicalRecord};
+        }
+        else {
+            MedicalRecord[] newArr = Arrays.copyOf(this.getMedicalRecords(), this.getMedicalRecords().length+1);
+            newArr[newArr.length-1] = medicalRecord;
+            this.medicalRecords = newArr;
+        }
     }
 }
