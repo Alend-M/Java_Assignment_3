314464d5df5f406d8e5c2e3cc1f74f45d4924180
Fatima4285 <144740887+Fatima4285@users.noreply.github.com>
2024-02-16 10:33:16 -0700

    Update ReliefService.java
    just added some short documentation

--- edu/ucalgary/oop/ReliefService.java
+++ edu/ucalgary/oop/ReliefService.java
@@ -1,3 +1,12 @@
+/**
+ * This is a class that organizes information regarding the Relief Services provided
+ * during an emergency. This information incldues who has called them, missing people, 
+ * the date of inquiry, information that is provided, and the last known location
+ * of the missing person
+*@author	Fatima Asif, Alend Maci, Marcus Lieu, Mohit Narula
+*@version	1.4
+*@since		1.0
+*/
 package edu.ucalgary.oop;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,7 +17,8 @@ public class ReliefService{
     private String dateOfInquiry;
     private String infoProvided;
     private Location lastKnownLocation;
-
+    
+    //constructor
     public ReliefService( Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry, 
     String infoProvided, Location lastKnownLocation){
         this.inquirer = inquirer;
@@ -17,7 +27,7 @@ public class ReliefService{
         this.infoProvided = infoProvided;
         this.lastKnownLocation = lastKnownLocation;
     }
-
+    //setters
     public void setInquirer( Inquirer inquirer){
         this.inquirer = inquirer;
     }
@@ -28,7 +38,7 @@ public class ReliefService{
 
     public void setDateOfInquiry(String dateOfInquiry) throws IllegalArgumentException{
         if (dateOfInquiry.matches("\\d{4}-\\d{2}-\\d{2}")) {
-            this.dateOfInquiry = dateOfInquiry;
+            this.dateOfInquiry = dateOfInquiry;//if date doesnt match this format, throw and IllegalArgumentException
         }
         else{
             throw new IllegalArgumentException("Invalid date format: " + dateOfInquiry);
@@ -43,7 +53,8 @@ public class ReliefService{
     public void setLastKnownLocation(Location lastKnownLocation){
         this.lastKnownLocation = lastKnownLocation;
     }
-
+    
+    //getters
     public Inquirer getInquirer(){
         return inquirer;
     }
